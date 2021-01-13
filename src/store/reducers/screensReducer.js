0d2b92d869a9c01f0acf60e7fa08ecb34b5025f3
rs8030 <rs8030@intl.att.com>
2021-01-13 12:41:15 +0200

    WIP

--- src/store/reducers/screensReducer.js
+++ src/store/reducers/screensReducer.js
@@ -1,19 +1,37 @@
 import actionsTypes from '../actionsTypes';
+import createReducer from './createReducer';
 
 const initialState = {
-  currentScreen: 'Begin',
+  currentScreen: '',
 };
 
-const screensReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionsTypes.SET_CURRENT_SCREEN:
-      return {
-        ...state,
-        currentScreen: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+const screensReducer = createReducer(initialState, {
+  [actionsTypes.SET_CURRENT_SCREEN]: (state, {payload}) => {
+    return {
+      ...state,
+      currentScreen: payload
+    };
+  },
+  [actionsTypes.OPEN_NOTIFICATION]: (state, {payload}) => {
+    return {
+      ...state,
+      isNotificationOpen: true,
+      notificationMessage: payload
+    };
+  },
+  [actionsTypes.CLOSE_NOTIFICATION]: (state) => {
+    return {
+      ...state,
+      isNotificationOpen: false,
+      notificationMessage: ''
+    };
+  },
+  [actionsTypes.TOGGLE_ERROR_WINDOW_IS_OPEN]: state => {
+    return {
+      ...state,
+      isErrorWindowOpen: !state.isErrorWindowOpen
+    };
+  },
+});
 
 export default screensReducer;
