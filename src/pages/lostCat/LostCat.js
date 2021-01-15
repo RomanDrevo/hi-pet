1e0b8edc02fd8db99ffaf611d82bccaed0fc9bfe
@vadim17 <vadim@genesort.com>
2021-01-15 19:28:05 +0200

    grid layout

--- src/pages/lostCat/LostCat.js
+++ src/pages/lostCat/LostCat.js
@@ -3,7 +3,13 @@ import styles from './LostCat.module.scss';
 
 const LostCat = (props) => {
   return (
-    <div className={styles['lost-cat-wrapper']}>LostCat</div>
+    <div className={styles['lost-cat-wrapper']}>
+      <div className="action-layout" />
+      <div className='images-layout'/>
+      <div className='where-lost-layout'/>
+      <div className='color-patterns-layout'/>
+      <div className='additional-layout'/>
+    </div>
   );
 };
 
